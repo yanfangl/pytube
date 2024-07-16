60f18cf002f94d3c34a630b38afd1e9394fa37ac
yanfangl <46511053+yanfangl@users.noreply.github.com>
2024-07-16 13:27:47 +0800

    Update innertube.py

--- pytube/innertube.py
+++ pytube/innertube.py
@@ -45,7 +45,7 @@ _default_clients = {
         'context': {
             'client': {
                 'clientName': 'ANDROID',
-                'clientVersion': '17.31.35',
+                'clientVersion': '19.08.35',
                 'androidSdkVersion': 30
             }
         },
@@ -58,7 +58,7 @@ _default_clients = {
         'context': {
             'client': {
                 'clientName': 'IOS',
-                'clientVersion': '17.33.2',
+                'clientVersion': '19.08.35',
                 'deviceModel': 'iPhone14,3'
             }
         },
@@ -85,7 +85,7 @@ _default_clients = {
         'context': {
             'client': {
                 'clientName': 'ANDROID_EMBEDDED_PLAYER',
-                'clientVersion': '17.31.35',
+                'clientVersion': '19.08.35',
                 'clientScreen': 'EMBED',
                 'androidSdkVersion': 30,
             }
@@ -99,7 +99,7 @@ _default_clients = {
         'context': {
             'client': {
                 'clientName': 'IOS_MESSAGES_EXTENSION',
-                'clientVersion': '17.33.2',
+                'clientVersion': '19.08.35',
                 'deviceModel': 'iPhone14,3'
             }
         },
@@ -125,7 +125,7 @@ _default_clients = {
         'context': {
             'client': {
                 'clientName': 'ANDROID_MUSIC',
-                'clientVersion': '5.16.51',
+                'clientVersion': '6.40.52',
                 'androidSdkVersion': 30
             }
         },
@@ -138,7 +138,7 @@ _default_clients = {
         'context': {
             'client': {
                 'clientName': 'IOS_MUSIC',
-                'clientVersion': '5.21',
+                'clientVersion': '6.41',
                 'deviceModel': 'iPhone14,3'
             }
         },
